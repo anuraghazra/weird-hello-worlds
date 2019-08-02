c7eb66803bf2c64c2f3cd944c62cb0e8c684ecc1
anuraghazra <hazru.anurag@gmail.com>
2019-08-02 21:35:06 +0530

    initial commit

---
+++ hello_worlds.js
@@ -0,0 +1,29 @@
+// weird `hello world` by github.com/anuraghazra
+
+// Submited By Anurag Hazra
+- async function(_, __, ___) {
+  for (; _ >=0 ;) {
+    {{{{{{{{{{{{{{{{{{{{{{{{{{{{{
+    {
+      with(__) {
+        if (_) {
+          try {
+            throw new Error();
+          } catch (_) {} finally {
+            while(_--) {
+              _ = () => new Promise(r => ___(() => r('hello weird world'), 1));
+              __(await _());
+            }
+          }
+        }
+      }
+    }
+    }}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+  }
+}(1, console.log, setTimeout);
+
+
+// ----- Your Contributions Here ----- (add one by one)
+
+// Submited By YOUR_NAME
+// your weird hello world code here
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Weird Hello Worlds
+
+Lets write some weird Hello worlds. do something crazy, complicated, weird, and amazing!!
+
+![Weird Hello Worlds](./codeshots/helloworld_1-2x.png)
+
+
+### Contributions are welcome.
+
+just open the **hello_worlds.js** file and add your contribution at the end of the file with your name
+
+```js
+// hello_worlds.js --
+
+...
+
+// Submited By YOUR_NAME
+console.log('hello weird world')
+
+```
+
+Made with craziness and :heart:
